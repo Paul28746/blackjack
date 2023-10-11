cd638668702d41ad2855ea7a546e23be0ce0b923
Paul28746 <141688597+Paul28746@users.noreply.github.com>
2023-10-11 20:45:10 +0100

    Message.java

--- Message.java
+++ Message.java
@@ -1,1 +1,22 @@
+package blackjack;
+
+public class Message {
+	
+	String mes = "empty_for_now";
+	String who = "nobody_for_now";
+	
+	public Message(String m, String w) {
+		this.mes = m;
+		this.who = w;
+	}
+	
+	public String getMessage() {
+		return this.mes;
+	}
+	
+	public String getWho() {
+		return this.who;
+	}
+	
+}
 
